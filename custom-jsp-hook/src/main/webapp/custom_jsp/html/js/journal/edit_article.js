8948fb489d4d8167f140ed5ffee0cbe3459d58a0
Damiano Seno <dax86it@gmail.com>
2016-08-08 10:33:08 +0200

    Fixes error if custom fields are present.

--- custom-jsp-hook/src/main/webapp/custom_jsp/html/js/journal/edit_article.js
+++ custom-jsp-hook/src/main/webapp/custom_jsp/html/js/journal/edit_article.js
@@ -1,4 +1,4 @@
-var fieldWrapperSelector = '.control-group.field-wrapper';
+var fieldWrapperSelector = '#_15_content .control-group.field-wrapper';
 var fieldsDisplaySelector = '#_15__fieldsDisplay';
 
 initSort();
